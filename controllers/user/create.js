0f6719ad6e888d2641ea8f2ad5f4bde2d5cf058b
Patrick Reynolds <reynolds@adobe.com>
2015-02-03 23:55:35 -0800

    Fix new user bug signup bug when creating a new user.

--- controllers/user/create.js
+++ controllers/user/create.js
@@ -6,17 +6,17 @@ module.exports = function(req, res, next) {
     var newUser = {
     }
 
-    user.username  = req.body.username || user.username
-    user.password  = req.body.password || user.password
-    user.firstName = req.body.firstName || user.firstName
-    user.lastName  = req.body.lastName || user.lastName
+    newUser.username  = req.body.username
+    newUser.password  = req.body.password
+    newUser.firstName = req.body.firstName
+    newUser.lastName  = req.body.lastName
     newUser.updatedAt =  Date.now()
     newUser.createdAt =  Date.now()
-    user.instagramAccessToken = req.instagramAccessToken || user.instagramAccessToken
+    newUser.instagramAccessToken = req.instagramAccessToken
 
     // Inserting a new task into MongoDB
     // via Mongoose create method.
-    User.create(newUser, function(err) {
+    User.create(newUser, function(err, user) {
         if (err) { 
             res.json({
                 status: 400,
@@ -25,7 +25,8 @@ module.exports = function(req, res, next) {
         } else {
             console.log("Created User: " + newUser.username);
             res.json({
-                status: 200
+                status: 200,
+                user: user
             })
         }
     })
